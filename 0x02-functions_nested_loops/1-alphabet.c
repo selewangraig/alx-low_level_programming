40989d4c0448123aa084bb5ac8fc21e887e49056
Graig Selewan <gselewan@gmail.com>
2022-07-05 05:02:43 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print alphabet in lowercase
+ * followed by new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char al;
+for(al = 'a'; ch <= 'z'; al++)
+{
+putchar(al);
+}
+return (0);
+}

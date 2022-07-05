82c4ed229782872e3bce65d226fa3b2b492651e2
Graig Selewan <gselewan@gmail.com>
2022-07-05 03:50:28 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+include < stdio.h >
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+putchar(_);
+putchar(p);
+putchar(u);
+putchar(t);
+putchar(c);
+putchar(h);
+putchar(a);
+putchar(r\n);
+}

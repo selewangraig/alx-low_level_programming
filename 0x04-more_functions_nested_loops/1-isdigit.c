abbeb703848ec5b72647693648e9de4ff885cb60
Graig Selewan <gselewan@gmail.com>
2022-07-07 03:37:54 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,15 +2,13 @@
 
 /**
  * main - function that checks for digit (o through 9)
- *
- * Return: Always 0 (Success)
+ * @c: digit to be checked
+ * Return: 1 if digit, 0 if otherwise
  */
 
 int _isdigit(int c)
 {
 if (c >= 0 && c <= 9)
-{
 return (1);
-}
 return (0);
 }

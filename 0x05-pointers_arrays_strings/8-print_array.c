3de2074eb7a6025ee234bde3cab704b7ad762af5
Graig Selewan <gselewan@gmail.com>
2022-07-11 05:03:59 -0700

    Fixed compilation error

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,20 +1,30 @@
 #include "main.h"
 
 /**
- * print_array - prints n elements of an array of integers
- * @a: array
- * @n: number of elements
+ *print_array - prints n elements of an array of integers
+ *@a: A pointer to an int that will be updated/ changed
+ *@n: return value n
+ *Return: void which ,means our answer is correct
  */
 
 void print_array(int *a, int n)
+
 {
 int i;
-for (i = 0; i < n; i++)
+
+i = 0;
+while (i < n)
 {
-if (i != (n - 1))
-printf("%d, ", a[i]);
-else
 printf("%d", a[i]);
+
+if (i < n - 1)
+{
+printf(", ");
 }
-printf("\n");
+
+i++;
+}
+
+printf("\n")
+;
 }

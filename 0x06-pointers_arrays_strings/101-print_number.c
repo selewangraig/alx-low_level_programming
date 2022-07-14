e7f205cfabf0ca3ecf058e2aa327061ba9d216cd
Graig Selewan <gselewan@gmail.com>
2022-07-14 01:42:17 -0700

    Fixed main function

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,35 +1,25 @@
 #include "main.h"
 
 /**
- * print_number - print an integer, without using long, arrays, or pointers
- * @n: number to be printed
+ * print_number - prints an integer;
+ * @n: integer to be printed;
  */
 
 void print_number(int n)
 {
-unsigned int tens, digit, positive = n;
-double t_beg = 1;
-
-if (n == 0)
-_putchar('0');
-else
-{
+unsigned int n1;
 if (n < 0)
 {
-positive = n * -1;
+n1 = -n;
 _putchar('-');
 }
-
-while (t_beg <= positive)
-t_beg *= 10;
-tens = t_beg / 10;
-
-while (tens >= 1)
+else
 {
-digit = positive / tens;
-_putchar(digit + '0');
-positive = (positive - (tens *digit));
-tens /= 10;
+n1 = n;
 }
-}
+
+if (n1 / 10)
+print_number(n1 / 10);
+
+_putchar((n1 % 10) + '0');
 }

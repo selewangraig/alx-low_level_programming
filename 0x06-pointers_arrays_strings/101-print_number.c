e4442f1256ed9d3972ebcee7452d364eb48e7abb
Graig Selewan <gselewan@gmail.com>
2022-07-14 02:09:53 -0700

    Fixed main function

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,49 @@
-#include "main.h"
-
+include "main.h"
+#include <stdio.h>
 /**
- * print_number - prints an integer
- * @n: integer to be printed
+ * print_number - Print an integer using only _putchar
+ * @n: integer to print
  */
 
 void print_number(int n)
 {
+int power;
+int neg;
+int hold;
 
-unsigned int num = n;
-
+neg = 0;
+power = 1;
+hold = n;
 if (n < 0)
 {
 _putchar('-');
-num = -num;
+neg = 1;
 }
 
-if ((num / 10) > 0)
-print_number(num / 10);
+while (hold > 9 || hold < -9)
+{
+power *= 10;
+hold /= 10;
+}
 
-_putchar((num % 10) + '0');
+while (power > 0)
+{
+if (power > 9)
+{
+if (!neg)
+_putchar((n / power % 10) + '0');
+else
+_putchar((n / power % 10) * -1 + '0');
+
+power /= 10;
+}
+if (power == 1)
+{
+if (neg)
+_putchar((n % 10) * -1 + '0');
+else
+_putchar(n % 10 + '0');
+power = 0;
+}
+}
 }

e3367f46bd67a2b93e5aed73935af82cb91c9cc0
Graig Selewan <gselewan@gmail.com>
2022-07-14 03:14:32 -0700

    Advanced

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifdef MAIN_H
 #define MAIN_H
 
-int _putchar(char);
+int _putchar(char n);
 char *_strcat(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);

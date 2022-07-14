1665be53120e1f25247d454d71c2b75151161f6f
Graig Selewan <gselewan@gmail.com>
2022-07-14 02:20:53 -0700

    Fixed prototype

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifdef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+int _putchar(char);
 char *_strcat(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);

f3305d75b8cd711521def4631ad1c8a500a2df8b
Graig Selewan <gselewan@gmail.com>
2022-07-20 09:22:34 -0700

    changed function

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * factorial - find factorial of n
- * @n: number
- * Return: factorial of number, -1 if negative n
+ * _strlen_recursion - return string length
+ * @s: string
+ * Return: string length
  */
 
-int factorial(int n)
+int _strlen_recursion(char *s)
 {
-if (n < 0)
-return (-1);
-else if (n == 0 || n == 1)
-return (n);
-else
-return (n * factorial(n - 1));
+if (*s == '\0')
+return (0);
+
+s++;
+return (1 + _strlen_recursion(s));
 }

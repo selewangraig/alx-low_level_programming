1cc0cdef1b042987ca56c42f0c5a4b4ccb3d19fd
Graig Selewan <gselewan@gmail.com>
2022-08-01 13:29:34 -0700

    fixed main function

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
+#ifndef FILE_ABS
+#define FILE_ABS
 
-#define ABS(x) (((x) < 0) ? (-x) : (x))
+#define ABS(X) ((X) < 0 ? -1 * (X) : (X))
 
 #endif

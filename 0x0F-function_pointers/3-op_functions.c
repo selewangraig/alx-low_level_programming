da68527807038b73e7e293372edc3850509f5dff
Graig Selewan <gselewan@gmail.com>
2022-08-03 14:44:17 -0700

    fixed betty style error

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -6,7 +6,7 @@
  */
 int op_add(int a, int b)
 {
-  return (a + b);
+return (a + b);
 }
 
 /**
@@ -17,7 +17,7 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-  return (a - b);
+return (a - b);
 }
 
 /**
@@ -28,7 +28,7 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
-  return (a * b);
+return (a * b);
 }
 
 /**

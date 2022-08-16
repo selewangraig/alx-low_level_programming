5d55bb4808730f5506c3bf139c11b1f35dba641f
Graig Selewan <gselewan@gmail.com>
2022-08-16 04:36:41 -0700

    added new function

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -20,6 +20,20 @@ struct listint_s *next;
 }
 listint_t;
 
+/**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ *
+ * Description: singly linked list of pointers
+ */
+typedef struct listp_s
+{
+  void *p;
+  struct listp_s *next;
+}
+listp_t;
+
 /* function prototypes */
 int _putchar(char c);
 size_t print_listint(const listint_t *h);

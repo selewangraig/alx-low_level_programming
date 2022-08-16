9b23a98f74b0ee909dcbc73249f87690aae95c55
Graig Selewan <gselewan@gmail.com>
2022-08-16 02:51:51 -0700

    initial commit

---
+++ 0x13-more_singly_linked_lists/_putchar.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * File: _putchar.c
+ *
+ * Author: Felix Adeh Essienne
+ *
+ * Description - Writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
